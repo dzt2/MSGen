ee4a47b79c4017652db5779bf1ce51981a482b24
欢 林 <643729712@qq.com>
2017-06-14 14:53:44 +0800

    +outputter

--- MSGen/submut.cpp
+++ MSGen/submut.cpp
@@ -5,10 +5,12 @@ TypedMutantSet::TypedMutantSet(MSGraph & g) : graph(g),
 	
 	MutantSpace & mspace = mutants.get_space();
 	stubborn_mutants = mspace.create_set();
+	hard_mutants = mspace.create_set();
 	subsuming_mutants = mspace.create_set();
 	subsumed_mutants = mspace.create_set();
 
 	stubborn_cluster = nullptr;
+	hard_clusters.clear();
 	subsuming_clusters.clear();
 	subsumed_clusters.clear();
 
@@ -50,6 +52,16 @@ TypedMutantSet::TypedMutantSet(MSGraph & g) : graph(g),
 		}
 	}
 
+	const MuHierarchy & hierarchy = graph.get_hierarchy();
+	size_t degree = hierarchy.get_degrees()[0];
+	if (stubborn_cluster != nullptr) degree = hierarchy.get_degrees()[1];
+	const std::set<MuCluster *> & H1 = hierarchy.get_clusters_of(degree);
+	auto hbeg = H1.begin(), hend = H1.end();
+	while (hbeg != hend) {
+		MuCluster * cluster = *(hbeg++);
+		hard_clusters.insert(cluster);
+	}
+
 	Mutant::ID mid = 0, mnum = mspace.number_of_mutants();
 	while (mid < mnum) {
 		if (graph.has_cluster_of(mid)) {
@@ -58,6 +70,11 @@ TypedMutantSet::TypedMutantSet(MSGraph & g) : graph(g),
 			/* stubborn mutant */
 			if (&cluster == stubborn_cluster) {
 				stubborn_mutants->add_mutant(mid);
+			}
+			/* hard mutants in H[1] (kulled by least tests) */
+			else if (hard_clusters.count(&cluster) > 0) {
+				hard_mutants->add_mutant(mid);
+				subsuming_mutants->add_mutant(mid);
 			}
 			/* subsuming mutants */
 			else if (subsuming_clusters.count(&cluster) > 0) {
@@ -85,7 +102,303 @@ TypedMutantSet::~TypedMutantSet() {
 	mspace.delete_set(stubborn_mutants);
 	mspace.delete_set(subsuming_mutants);
 	mspace.delete_set(subsumed_mutants);
+	mspace.delete_set(hard_mutants);
+
+	subsuming_clusters.clear();
+	hard_clusters.clear();
+	subsumed_clusters.clear();
 }
+
+void TypedOutputter::open(const File & root) {
+	close();	/* close the outputter */
+
+	/* iterate the root directory */
+	if (root.is_directory()) {
+		const std::vector<File *> & files = root.list_files();
+		for (int i = 0; i < files.size(); i++) {
+			File & file = *(files[i]);
+			if (file.is_directory()) {
+				const std::string & name
+					= file.get_local_name();
+				if (name == "analysis") {
+					dir = &file; break;
+				}
+			}
+		}
+	}
+
+	/* validation */
+	if (dir == nullptr) {
+		CError error(CErrorType::InvalidArguments, 
+			"TypedOutputter::open", "no directory ../analysis is found");
+		CErrorConsumer::consume(error); exit(CErrorType::InvalidArguments);
+	}
+}
+void TypedOutputter::output_mutants(const TypedMutantSet & tmutants) {
+	std::ofstream out1(dir->get_path() + "/stubborn_mutants.txt");
+	output_mutants(tmutants.get_stubborn_mutants(), 
+		tmutants.get_graph(), out1); out1.close();
+
+	std::ofstream out2(dir->get_path() + "/hard_mutants.txt");
+	output_mutants(tmutants.get_hard_mutants(), 
+		tmutants.get_graph(), out2); out2.close();
+
+	std::ofstream out3(dir->get_path() + "/subsuming_mutants.txt");
+	output_mutants(tmutants.get_subsuming_mutants(), 
+		tmutants.get_graph(), out3); out3.close();
+}
+void TypedOutputter::output_distribution(const TypedMutantSet & tmutants) {
+	std::ofstream out1(dir->get_path() + "/mutant_operator.txt");
+	output_distribute_operator(tmutants, out1); out1.close();
+
+	std::ofstream out2(dir->get_path() + "/mutant_location.txt");
+	output_distribute_location(tmutants, out2); out2.close();
+
+	std::ofstream out3(dir->get_path() + "/mutant_operator_location.txt");
+	output_distribute_operator_location(tmutants, out3); out3.close();
+}
+void TypedOutputter::output_mutants(const MutantSet & mutants, 
+	/* declarations */
+	const MSGraph & graph, std::ostream & out) {
+	MutantSpace & mspace = mutants.get_space();
+	const TextBuild & text = *(mspace.get_code_file().get_text());
+
+	/* iterate all the mutants in the set */
+	Mutant::ID mid, num = mspace.number_of_mutants();
+	for (mid = 0; mid < num; mid++) {
+		if (mutants.has_mutant(mid)) {
+			/* get the next mutants */
+			Mutant & mutant = mspace.get_mutant(mid);
+			const std::string & oprt = mutant.get_operator();
+			MuCluster & cluster = graph.get_cluster_of(mid);
+
+			/* mutant-summary */
+			out << "Mutant (" << mutant.get_id() << ")\n";
+			out << "\toperator: " << oprt << "\n";
+			out << "\tcluster: " << cluster.get_id() << "\n";
+
+			/* mutations for mutant */
+			for (int i = 0; i < mutant.get_orders(); i++) {
+				const Mutation & mutation = mutant.get_mutation(i);
+				const CodeLocation & location = mutation.get_location();
+
+				out << "\tmutation[" << i << "]:\n";
+				out << "\t\t-location: " << 
+					text.lineOfIndex(location.get_bias()) << " line\n";
+				out << "\t\t-original: \"" << location.get_text_at() << "\"\n";
+				out << "\t\t-replace: \"" << mutation.get_replacement() << "\"\n";
+			}
+
+			out << "\n";	/* end line */
+		}
+	}
+}
+void TypedOutputter::output_distribute_operator(
+	const TypedMutantSet & tmutants, std::ostream & out) {
+	/* declarations */
+	const MutantSet & all_mutants = tmutants.get_mutants();
+	const MutantSet & stubborn_mutants = tmutants.get_stubborn_mutants();
+	const MutantSet & subsuming_mutants = tmutants.get_subsuming_mutants();
+	std::map<std::string, int *> oprt_numbers;
+
+	const MutantSpace & mspace = all_mutants.get_space();
+	Mutant::ID mid, num = mspace.number_of_mutants();
+	for (mid = 0; mid < num; mid++) {
+		/* get next mutant */
+		Mutant & mutant = mspace.get_mutant(mid);
+		const std::string & oprt = mutant.get_operator();
+
+		/* get the numbers for counting */
+		int * numbers;
+		if (oprt_numbers.count(oprt) == 0) {
+			numbers = new int[3];
+			numbers[0] = 0; 
+			numbers[1] = 0;
+			numbers[2] = 0; 
+			oprt_numbers[oprt] = numbers;
+		}
+		else {
+			auto iter = oprt_numbers.find(oprt);
+			numbers = iter->second;
+		}
+
+		/* count the numbers */
+		if (all_mutants.has_mutant(mid)) {
+			/* stubborn mutants */
+			if (stubborn_mutants.has_mutant(mid))
+				numbers[0]++;
+			/* subsuming mutants */
+			else if (subsuming_mutants.has_mutant(mid))
+				numbers[1]++;
+			/* subsumed mutants */
+			else numbers[2]++;	
+		}
+	}
+
+	/* output operators */
+	out << "operator\tStubborn\tSubsuming\tSubsumed\tTotal\n";
+	auto beg = oprt_numbers.begin(), end = oprt_numbers.end();
+	while (beg != end) {
+		const std::string & oprt = beg->first;
+		int * numbers = beg->second; beg++;
+
+		out << oprt << "\t" << numbers[0] << "\t";
+		out << numbers[1] << "\t" << numbers[2] << "\t";
+		out << numbers[0] + numbers[1] + numbers[2] << "\n";
+
+		delete numbers;
+	}
+	out << std::endl;
+}
+void TypedOutputter::output_distribute_location(
+	const TypedMutantSet & tmutants, std::ostream & out) {
+	/* declarations */
+	const MutantSet & all_mutants = tmutants.get_mutants();
+	const MutantSet & stubborn_mutants = tmutants.get_stubborn_mutants();
+	const MutantSet & subsuming_mutants = tmutants.get_subsuming_mutants();
+	std::map<std::string, int *> key_numbers;
+	std::map<std::string, const CodeLocation *> key_locations;
+
+	/* count the numbers */
+	const MutantSpace & mspace = all_mutants.get_space();
+	Mutant::ID mid, num = mspace.number_of_mutants();
+	for (mid = 0; mid < num; mid++) {
+		/* get next mutant and its location-key */
+		Mutant & mutant = mspace.get_mutant(mid);
+		const Mutation & mutation = 
+			mutant.get_mutation(mutant.get_orders() - 1);
+		const CodeLocation & location = mutation.get_location();
+		std::string key = std::to_string(location.get_bias());
+		key += ":" + std::to_string(location.get_length());
+
+		/* get numbers for counting */
+		int * numbers;
+		if (key_numbers.count(key) == 0) {
+			numbers = new int[3];
+			numbers[0] = 0;
+			numbers[1] = 0;
+			numbers[2] = 0;
+			key_numbers[key] = numbers;
+			key_locations[key] = &location;
+		}
+		else {
+			auto iter = key_numbers.find(key);
+			numbers = iter->second;
+		}
+
+		/* couting for mutants */
+		if (all_mutants.has_mutant(mid)) {
+			/* for stubborn mutants */
+			if (stubborn_mutants.has_mutant(mid))
+				numbers[0]++;
+			/* for subsuming mutants */
+			else if (subsuming_mutants.has_mutant(mid))
+				numbers[1]++;
+			/* for subsumed mutants */
+			else numbers[2]++;
+		}
+	} /* end for */
+
+	/* output lines */
+	const TextBuild & text = *(mspace.get_code_file().get_text());
+	out << "bias\tlength\tline\tstubborn\tsubsuming\tsubsumed\ttotal\n";
+	auto beg = key_numbers.begin(), end = key_numbers.end();
+	while (beg != end) {
+		/* get next line */
+		const std::string & key = beg->first;
+		int * numbers = beg->second; beg++;
+		auto iter = key_locations.find(key);
+		const CodeLocation & loc = *(iter->second);
+
+		/* output line */
+		out << loc.get_bias() << "\t" << loc.get_length() << "\t";
+		out << text.lineOfIndex(loc.get_bias()) << "\t" << numbers[0];
+		out << "\t" << numbers[1] << "\t" << numbers[2] << "\t";
+		out << numbers[0] + numbers[1] + numbers[2] << "\n";
+
+		delete numbers;
+	}
+	out << std::endl;
+}
+void TypedOutputter::output_distribute_operator_location(
+	const TypedMutantSet & tmutants, std::ostream & out) {
+	/* declarations */
+	const MutantSet & all_mutants = tmutants.get_mutants();
+	const MutantSet & stubborn_mutants = tmutants.get_stubborn_mutants();
+	const MutantSet & subsuming_mutants = tmutants.get_subsuming_mutants();
+	std::map<std::string, int *> key_numbers;
+	std::map<std::string, std::string> key_operators;
+	std::map<std::string, const CodeLocation *> key_locations;
+
+	/* count the numbers */
+	const MutantSpace & mspace = all_mutants.get_space();
+	Mutant::ID mid, num = mspace.number_of_mutants();
+	for (mid = 0; mid < num; mid++) {
+		/* get next mutant and operator-location-key */
+		Mutant & mutant = mspace.get_mutant(mid);
+		const Mutation & mutation =
+			mutant.get_mutation(mutant.get_orders() - 1);
+		const std::string oprt = mutant.get_operator();
+		const CodeLocation & location = mutation.get_location();
+		
+		/* get the key for this mutant */
+		std::string key = std::to_string(location.get_bias());
+		key += ":" + std::to_string(location.get_length());
+		key += ":" + oprt;
+
+		/* get numbers for counting */
+		int * numbers;
+		if (key_numbers.count(key) == 0) {
+			numbers = new int[3];
+			numbers[0] = 0;
+			numbers[1] = 0;
+			numbers[2] = 0;
+			key_numbers[key] = numbers;
+			key_operators[key] = oprt;
+			key_locations[key] = &location;
+		}
+		else {
+			auto iter = key_numbers.find(key);
+			numbers = iter->second;
+		}
+
+		/* couting for mutants */
+		if (all_mutants.has_mutant(mid)) {
+			/* for stubborn mutants */
+			if (stubborn_mutants.has_mutant(mid))
+				numbers[0]++;
+			/* for subsuming mutants */
+			else if (subsuming_mutants.has_mutant(mid))
+				numbers[1]++;
+			/* for subsumed mutants */
+			else numbers[2]++;
+		}
+	} /* end for */
+
+	/* output */
+	const TextBuild & text = *(mspace.get_code_file().get_text());
+	out << "operator\tbias\tlength\tline\tstubborn\tsubsuming\tsubsumed\ttotal\n";
+	auto beg = key_numbers.begin(), end = key_numbers.end();
+	while (beg != end) {
+		/* get next line */
+		const std::string & key = beg->first;
+		int * numbers = beg->second; beg++;
+		auto iter = key_locations.find(key);
+		const CodeLocation & loc = *(iter->second);
+		const std::string & oprt =
+			(key_operators.find(key)->second);
+
+		/* output line */
+		out << oprt << "\t" << loc.get_bias() << "\t" << loc.get_length() << "\t";
+		out << text.lineOfIndex(loc.get_bias()) << "\t" << numbers[0] << "\t";
+		out << numbers[1] << "\t" << numbers[2] << "\t";
+		out << numbers[0] + numbers[1] + numbers[2] << "\n";
+
+		delete numbers;	/* release numbers */
+	}
+	out << std::endl;
+}
+
 
 /* APIs for project models */
 /* load the tests and mutants into the project */
@@ -486,8 +799,8 @@ static void subsuming_mutants(const TypedMutantSet & tmutants, std::ostream & ou
 int main() {
 	// input-arguments
 	std::string prefix = "../../../MyData/SiemensSuite/"; 
-	std::string prname = "tcas"; 
-	TestType ttype = TestType::tcas;
+	std::string prname = "bubble"; 
+	TestType ttype = TestType::general;
 
 	// create code-project, mutant-project, test-project
 	File & root = *(new File(prefix + prname));
@@ -524,30 +837,10 @@ int main() {
 
 		// create TypedMutantSet
 		TypedMutantSet tmutants(graph);
-		out << "\nTyped Mutants Set:\n";
-		printTMS(tmutants, out);
-		out << std::endl;
-
-		// print classification
-		out << "Classification for subsuming mutants\n";
-		mutantsByOperator(tmutants.get_subsuming_mutants(), out);
-		mutantsByLocation(tmutants.get_subsuming_mutants(), out);
-
-		/* end output */ out.close();
-
-		// try to output cluster-operator
-		std::ofstream oprt_out(prefix + prname + "/mutant_operator.txt");
-		stubborn_operator(tmutants, oprt_out); oprt_out.close();
-		std::ofstream loc_out(prefix + prname + "/mutant_location.txt");
-		stubborn_location(tmutants, loc_out); loc_out.close();
-		std::ofstream oprt_loc_out(prefix + prname + "/mutant_operator_location.txt");
-		stubborn_location_operator(tmutants, oprt_loc_out); oprt_loc_out.close();
-
-		// output stubborn/subsuming mutants 
-		std::ofstream stub_out(prefix + prname + "/stubborn.txt");
-		stubborn_mutants(tmutants, stub_out); stub_out.close();
-		std::ofstream subs_out(prefix + prname + "/subsuming.txt");
-		subsuming_mutants(tmutants, subs_out); subs_out.close();
+		TypedOutputter tout; tout.open(root);
+		tout.output_mutants(tmutants);
+		tout.output_distribution(tmutants);
+		tout.close();
 	}
 
 	// delete memory
--- MSGen/submut.h
+++ MSGen/submut.h
@@ -13,11 +13,13 @@
 #include "mgraph.h"
 
 class TypedMutantSet;
+class TypedOutputter;
 
 /* Mutants in MSG are classified in three types:
 	I.	stubborn mutants: cannot be killed by any tests in finite T;
 	II. subsuming mutants: directly subsumed by stubborn mutants, i.e. non-redundant set of mutations;
 	III. subsumed mutants: subsumed by subsuming mutants, i.e. redundant mutants.
+	IV. H0-hard mutants: mutants in H[1] (with least non-zero degree)
 */
 class TypedMutantSet {
 public:
@@ -30,6 +32,8 @@ public:
 	const MutantSet & get_mutants() const { return mutants; }
 	/* get the stubborn mutants */
 	const MutantSet & get_stubborn_mutants() const { return *stubborn_mutants; }
+	/* get the hard mutants (killed by least tests) */
+	const MutantSet & get_hard_mutants() const { return *hard_mutants; }
 	/* get the subsuming mutants (non-redundant) */
 	const MutantSet & get_subsuming_mutants() const { return *subsuming_mutants; }
 	/* get the subsumed mutants (redundant) */
@@ -39,6 +43,8 @@ public:
 	MSGraph & get_graph() const { return graph; }
 	/* get the cluster of stubborn mutants */
 	MuCluster & get_stubborn_cluster() const { return *stubborn_cluster; }
+	/* get the clusters for hard mutants (killed by least tests) */
+	const std::set<MuCluster *> & get_hard_clusters() const { return hard_clusters; }
 	/* get the clusters of subsuming mutants */
 	const std::set<MuCluster *> & get_subsuming_clusters() const { return subsuming_clusters; }
 	/* get the clusters for subsumed mutants */
@@ -52,6 +58,8 @@ private:
 
 	/* stubborn mutants (unkilled) */
 	MutantSet * stubborn_mutants;
+	/* hard mutants are killed by least tests */
+	MutantSet * hard_mutants;
 	/* subsuming mutants (non-redundant) */
 	MutantSet * subsuming_mutants;
 	/* subsumed mutants (redundant) */
@@ -59,8 +67,48 @@ private:
 
 	/* stubborn cluster (unkilled) */
 	MuCluster * stubborn_cluster;
+	/* set of clusters for hard mutants */
+	std::set<MuCluster *> hard_clusters;
 	/* subsuming clusters (minimal non-redundant) */
 	std::set<MuCluster *> subsuming_clusters;
 	/* subsumed clusters (redundant) */
 	std::set<MuCluster *> subsumed_clusters;
 };
+/* to output the statistical information for typed mutants */
+class TypedOutputter {
+public:
+	/* create outputter for typed mutants */
+	TypedOutputter() : dir(nullptr) {}
+	/* deconstructor */
+	~TypedOutputter() { close(); }
+
+	/* set the ../ where defines director of "analysis" for storing generated files */
+	void open(const File &);
+	/* to generate ../analysis/stubborn_mutants.txt | 
+		hard_mutants.txt | subsuming_mutants.txt */
+	void output_mutants(const TypedMutantSet &);
+	/* to generate ../analysis/dist_xxx.txt, including:
+		1) dist_operator.txt
+		2) dist_location.txt
+		3) dist_operator_location.txt
+	*/
+	void output_distribution(const TypedMutantSet &);
+	/* close the outputter */
+	void close() { dir = nullptr; }
+
+private:
+	/* ../analysis */
+	File * dir;
+
+protected:
+	/* output mutants to specified file */
+	void output_mutants(const MutantSet &, const MSGraph &, std::ostream &);
+	/* generate ../analysis/dist_operator.txt */
+	void output_distribute_operator(const TypedMutantSet &, std::ostream &);
+	/* generate ../analysis/dist_location.txt */
+	void output_distribute_location(const TypedMutantSet &, std::ostream &);
+	/* generate ../analysis/dist_oprt_location.txt */
+	void output_distribute_operator_location(const TypedMutantSet &, std::ostream &);
+};
+
+
